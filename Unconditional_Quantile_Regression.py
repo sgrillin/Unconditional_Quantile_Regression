f00d6e7566fb5b04e9ce8d450ec7a5a71c0b5e26
sgrillin <71425710+sgrillin@users.noreply.github.com>
2024-09-19 23:08:15 +0100

    Updated description of the functions

--- Unconditional_Quantile_Regression.py
+++ Unconditional_Quantile_Regression.py
@@ -5,20 +5,15 @@ from scipy.stats import gaussian_kde
 import statsmodels.api as sm
 
 
-# Step 1: Estimate quantiles of the outcome variable
 def estimate_quantiles(y, tau):
     """Estimates the quantile tau of y"""
     return np.percentile(y, tau * 100)
 
-
-# Step 2: Kernel density estimation for f(q_tau)
 def kernel_density_estimate(y, q_tau):
     """Estimate the density at q_tau using Gaussian kernel density estimator"""
     kde = gaussian_kde(y)
     return kde.evaluate(q_tau)[0]
 
-
-# Step 3: Compute RIF for the given quantile
 def rif_quantile(y, tau):
     """Compute the RIF for the quantile tau"""
     q_tau = estimate_quantiles(y, tau)
@@ -31,8 +26,6 @@ def rif_quantile(y, tau):
     rif = q_tau + (tau - indicator) / f_q_tau
     return rif
 
-
-# Step 4: Fit the RIF regression model
 def fit_rif_regression(y, X, tau):
     """Fit an OLS regression model for the RIF of a given quantile tau"""
     rif_y = rif_quantile(y, tau)
